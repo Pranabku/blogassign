ed6a9d48cd9532808ac424177c7f79fc166dfda4
pranabku <pranabku@gmail.com>
2022-04-07 15:34:59 +0530

    first commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-urlpatterns = [ path('',views.index)]
+urlpatterns = [ path('',views.index),
+                path('login',views.Login, name='login'),
+                ]
